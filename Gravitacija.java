9fb1529ff218239df9f64fd72483f512892827af
xao1215 <ap3338@student.uni-lj.si>
2020-02-25 16:54:47 +0000

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,8 @@ public class Gravitacija {
     public static void main(String[] args) { 
         System.out.println("OIS je zakon!....you massive cocksucker");
     }
+    
+    public static void izpis(double nad_visina, double pospesek){
+        System.out.println(nadvisina + " " + pospesek);
+    }
 }
