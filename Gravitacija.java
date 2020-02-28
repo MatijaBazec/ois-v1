892fcfc4c3d0ec12f36c4ff4ae59b44c91171c55
ArjanSkok <arjan.skok@gmail.com>
2020-02-28 19:07:11 +0000

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,24 @@
 public class Gravitacija {
+    
     public static void main(String[] args) { 
+        
+        
         System.out.println("OIS je zakon!....you massive cocksucker");
+        
     }
+    
+    public static double pospesek(int visina) {
+        
+        //izračunamo vse spremenljivke vrednosti
+        double r = visina + 6371000; //v kilometrih
+        double konstanta = 6.674 * Math.pow(10, -11);
+        double masa = 5.972 * Math.pow(10, 24);
+        
+        //izracunamo g
+        double g = konstanta * (masa / (r * r));
+        
+        
+        return g;
+    }
+    
 }
